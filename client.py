76a714abb83e7a80d4306ab10aa5198a11b3a348
lubro <github@lu-bro.de>
2020-07-27 08:26:55 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
+#!/usr/bin/python3
 import sys
-
 from pydbus import SessionBus
 
+# Getthe D-Bus connection to the RunAsService
 bus = SessionBus()
-notifications = bus.get('.RunAs')
+run_as_service = bus.get('.RunAs')
 
+# Parse commandline parameters.
+# The first is the name of the application to start.
+# All others are forwarded as parameters for the application.
+
+# No additional parameters by default
 parameter = ''
+# Parse if there are additional parameters
 if len(sys.argv) > 2:
     for item in sys.argv[2:]:
         parameter = f'{parameter} {item}'
-print(notifications.Run(sys.argv[1], parameter))
+
+# Print the value, returned by the RunAsService
+print(run_as_service.Run(sys.argv[1], parameter))
